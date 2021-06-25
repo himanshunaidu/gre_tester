44b3a520f013356e160912e7a4d1e6a710edd0a7
Himanshu First <himanshunaidu12161997@gmail.com>
2021-06-25 23:26:12 +0530

    Global strings

---
+++ src/strings.js
@@ -0,0 +1,9 @@
+//Type
+export const RANDOM = 'RANDOM';
+export const SEQUENTIAL = 'SEQUENTIAL';
+
+
+//Details
+export const MEANING = 'MEANING';
+export const MNEMONIC = 'MNEMONIC';
+export const SENTENCE = 'SENTENCE';
