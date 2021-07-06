d463275caf75e5e9a4b1f2e235b1db7c411a549d
Himanshu First <himanshunaidu12161997@gmail.com>
2021-07-06 22:28:07 +0530

    Word db details

---
+++ src/store/dbDetails/word_meanings.js
@@ -0,0 +1,14 @@
+//Table Name
+export const WORD_MEANINGS="word_meanings";
+
+//word_meanings columns
+export const WID="wid";
+export const WORD="word";
+export const MNEMONIC="mnemonic";
+export const EXAMPLE="example";
+export const VERB="Verb";
+export const ADJECTIVE="Adjective";
+export const NOUN="Noun";
+export const ADVERB="Adverb";
+export const OTHERS="Others";
+export const MAIN_ID="main_id";
