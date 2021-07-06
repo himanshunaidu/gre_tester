5fb89f02f71f6e396bb8dfba7f257de31a4ddeb5
Himanshu First <himanshunaidu12161997@gmail.com>
2021-07-06 22:26:29 +0530

    Redux Action specify types

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,12 @@
+export const GET_TOTAL_WORDS = 'GET_TOTAL_WORDS';
+export const SUBMIT_TYPE = 'SUBMIT_TYPE';
+
+export const SUBMIT_DETAILS = 'SUBMIT_DETAILS';
+export const UPDATE_INDEX = 'UPDATE_INDEX';
+export const UPDATE_MEANING = 'UPDATE_MEANING';
+export const UPDATE_MNEMONIC = 'UPDATE_MNEMONIC';
+export const UPDATE_SENTENCE = 'UPDATE_SENTENCE';
+
+export const FAILED_WORDS = 'FAILED_WORDS';
+
+export const RANDOM_WORD = 'RANDOM_WORD';
