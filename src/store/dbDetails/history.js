59e2baa3b78a50a8e182969a81fb70bd3e1f8b68
Himanshu First <himanshunaidu12161997@gmail.com>
2021-07-06 22:27:53 +0530

    History db details

---
+++ src/store/dbDetails/history.js
@@ -0,0 +1,15 @@
+//Table Name
+export const HISTORY="history";
+
+//history columns
+export const HISTORY_ID="history_id";
+export const NAME="name";
+export const RANDOM="random";
+export const INDEX_START="index_start";
+export const MEANING="meaning";
+export const MEANING_SC="meaning_sc";
+export const MNEMONIC="mnemonic";
+export const MNEMONIC_SC="mnemonic_sc";
+export const SENTENCE="sentence";
+export const SENTENCE_SC="sentence_sc";
+export const TOTAL="total";
