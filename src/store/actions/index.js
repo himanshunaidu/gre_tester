194fe4972c2202283ff2083faed704326cbfb0a3
Himanshu First <himanshunaidu12161997@gmail.com>
2021-07-06 22:27:33 +0530

    Export Redux actions

---
+++ src/store/actions/index.js
@@ -0,0 +1,11 @@
+export {
+    getTotalWords,
+    typeSubmit,
+    indexUpdate,
+    meaningUpdate,
+    mnemonicUpdate,
+    sentenceUpdate,
+    detailsSubmit,
+    appendFailedWord,
+    removeFromRandomList
+} from './test'
