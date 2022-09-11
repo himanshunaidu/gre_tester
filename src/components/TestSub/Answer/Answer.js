a3ad5db209825baa9beac16cd3d55fe7b7aff58f
Himanshu Naidu <himanshunaidu12161997@gmail.com>
2022-09-11 12:33:36 +0530

    Adding Select All button to Answer component

--- src/components/TestSub/Answer/Answer.js
+++ src/components/TestSub/Answer/Answer.js
@@ -1,41 +1,135 @@
-import React, {Component} from 'react';
-import styles from './Answer.module.css';
-import PropTypes from 'prop-types';
-import * as strings from '../../../strings';
+import React, { Component } from "react";
+import styles from "./Answer.module.css";
+import PropTypes from "prop-types";
+import * as strings from "../../../strings";
+
+import Button from "../../UI/Button/Button";
+import * as ButtonClasses from "../../UI/Button/ButtonClasses";
 
 class Answer extends Component {
-    render() {
+  state = {
+    meaningSelected: false,
+    mnemonicSelected: false,
+    sentenceSelected: false,
+  };
 
-        let meaning = null, mnemonic = null, sentence = null;
-        if (this.props.meaning) {
-            meaning = <div>{strings.MEANING}: {this.props.meaning_ans} 
-                        <input type="checkbox" onChange={this.props.correctionHandler.bind(this, this.props.meaning_cor, strings.MEANING)}/>
-                    </div>;
-        }
-        if (this.props.mnemonic) {
-            mnemonic = <div>{strings.MNEMONIC}: {this.props.mnemonic_ans}
-                        <input type="checkbox" onChange={this.props.correctionHandler.bind(this, this.props.mnemonic_cor, strings.MNEMONIC)}/>
-                    </div>;
-        }
-        if (this.props.sentence) {
-            sentence = <div>{strings.SENTENCE}: {this.props.sentence_ans}
-                        <input type="checkbox" onChange={this.props.correctionHandler.bind(this, this.props.sentence_cor, strings.SENTENCE)}/>
-                    </div>;
-        }
-
-
-        return (
-            <div className={styles.Answer}>
-                <div><p>Answer</p></div>
-                
-                {meaning}
-                <br></br>
-                {mnemonic}
-                <br></br>
-                {sentence}
-            </div>
-        )
+  allCorrect = () => {
+    let meaningSelected = true;
+    let mnemonicSelected = true;
+    let sentenceSelected = true;
+    if (
+      this.state.meaningSelected &&
+      this.state.mnemonicSelected &&
+      this.state.sentenceSelected
+    ) {
+      meaningSelected = false;
+      mnemonicSelected = false;
+      sentenceSelected = false;
     }
+    this.setState({
+      meaningSelected: meaningSelected,
+      mnemonicSelected: mnemonicSelected,
+      sentenceSelected: sentenceSelected,
+    });
+    this.props.correctionHandler.call(this, meaningSelected, strings.MEANING);
+    this.props.correctionHandler.call(this, mnemonicSelected, strings.MNEMONIC);
+    this.props.correctionHandler.call(this, sentenceSelected, strings.SENTENCE);
+  };
+
+  render() {
+    let meaning = null,
+      mnemonic = null,
+      sentence = null;
+
+    if (this.props.meaning) {
+      meaning = (
+        <div className={styles.AnswerComponent}>
+          <input
+            type="checkbox"
+            checked={this.state.meaningSelected}
+            onChange={(e) => {
+              const meaningSelected = this.state.meaningSelected;
+              this.setState({
+                meaningSelected: !meaningSelected,
+              });
+              this.props.correctionHandler.call(
+                this,
+                !this.state.meaningSelected,
+                strings.MEANING
+              );
+            }}
+          />
+          {strings.MEANING}: {this.props.meaning_ans}
+        </div>
+      );
+    }
+
+    if (this.props.mnemonic) {
+      mnemonic = (
+        <div className={styles.AnswerComponent}>
+          <input
+            type="checkbox"
+            checked={this.state.mnemonicSelected}
+            onChange={(e) => {
+              const mnemonicSelected = this.state.mnemonicSelected;
+              this.setState({
+                mnemonicSelected: !mnemonicSelected,
+              });
+              this.props.correctionHandler.call(
+                this,
+                !this.props.mnemonicSelected,
+                strings.MNEMONIC
+              );
+            }}
+          />
+          {strings.MNEMONIC}: {this.props.mnemonic_ans}
+        </div>
+      );
+    }
+
+    if (this.props.sentence) {
+      sentence = (
+        <div className={styles.AnswerComponent}>
+          <input
+            type="checkbox"
+            checked={this.state.sentenceSelected}
+            onChange={(e) => {
+              const sentenceSelected = this.state.sentenceSelected;
+              this.setState({
+                sentenceSelected: !sentenceSelected,
+              });
+              this.props.correctionHandler.call(
+                this,
+                !this.state.sentenceSelected,
+                strings.SENTENCE
+              );
+            }}
+          />
+          {strings.SENTENCE}: {this.props.sentence_ans}
+        </div>
+      );
+    }
+
+    return (
+      <div className={styles.AnswerContainer}>
+        <div>
+          <p>Answer</p>
+        </div>
+
+        <div className={styles.Answer}>
+          {meaning}
+          <br></br>
+          {mnemonic}
+          <br></br>
+          {sentence}
+        </div>
+
+        <Button btnType={ButtonClasses.SUCCESS} onClick={this.allCorrect}>
+          Select All as Correct (Incorrect if all Selected)
+        </Button>
+      </div>
+    );
+  }
 }
 
 /*Answer.propTypes = {
--- src/containers/Test/Test.js
+++ src/containers/Test/Test.js
@@ -1,30 +1,36 @@
-import React, { Component } from 'react';
-import './Test.css';
+import React, { Component } from "react";
+import "./Test.css";
 
-import { connect } from 'react-redux';
-import { useLocation, NavLink, Route, Router, withRouter, Redirect } from 'react-router-dom';
-import axios from '../../axios';
+import { connect } from "react-redux";
+import {
+  useLocation,
+  NavLink,
+  Route,
+  Router,
+  withRouter,
+  Redirect,
+} from "react-router-dom";
+import axios from "../../axios";
 
-import * as actions from '../../store/actions/index';
-import * as strings from '../../strings';
-import * as word_meanings_col from '../../store/dbDetails/word_meanings';
-import * as history_col from '../../store/dbDetails/history';
+import * as actions from "../../store/actions/index";
+import * as strings from "../../strings";
+import * as word_meanings_col from "../../store/dbDetails/word_meanings";
+import * as history_col from "../../store/dbDetails/history";
 
-import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
-import Question from '../../components/TestSub/Question/Question';
-import Answer from '../../components/TestSub/Answer/Answer';
-import TestHistory from '../../components/HistorySub/TestHistory/TestHistory';
-import Score from '../../components/TestSub/Score/Score';
+import Auxiliary from "../../hoc/Auxiliary/Auxiliary";
+import Question from "../../components/TestSub/Question/Question";
+import Answer from "../../components/TestSub/Answer/Answer";
+import TestHistory from "../../components/HistorySub/TestHistory/TestHistory";
+import Score from "../../components/TestSub/Score/Score";
 
-import Button from '../../components/UI/Button/Button'
-import * as ButtonClasses from '../../components/UI/Button/ButtonClasses';
-import Modal from '../../components/UI/Modal/Modal';
-import EndTest from '../../components/EndTest/EndTest';
+import Button from "../../components/UI/Button/Button";
+import * as ButtonClasses from "../../components/UI/Button/ButtonClasses";
+import Modal from "../../components/UI/Modal/Modal";
+import EndTest from "../../components/EndTest/EndTest";
 
 //Equivalent to BurgerBuilder of the Udemy project
 
 class Test extends Component {
-
   state = {
     submitted: false,
     submitting_test: false,
@@ -42,100 +48,117 @@ class Test extends Component {
     meaning_sc: 0,
     mnemonic_sc: 0,
     sentence_sc: 0,
-    total: 0
-  }
+    total: 0,
+  };
 
   getWord = (meaning_sc, mnemonic_sc, sentence_sc, total, index) => {
     const wordparams = {
       params: {
-        index: index
-      }
-    }
+        index: index,
+      },
+    };
 
-    console.log(index)
+    console.log(index);
 
     //Set all the state variables for the word being extracted
-    axios.get('/test/word', wordparams)
-      .then(result => {
-        console.log(result);
-        if (!result.data.success) {
-          this.submitSuccessHandler()
-          return;
-        }
-        else {
-          let word_meaning = result.data.word_meaning;
-          //console.log(word_meaning);
+    axios.get("/test/word", wordparams).then((result) => {
+      console.log(result);
+      if (!result.data.success) {
+        this.submitSuccessHandler();
+        return;
+      } else {
+        let word_meaning = result.data.word_meaning;
+        //console.log(word_meaning);
 
-          let meaning = word_meaning[word_meanings_col.VERB]+ '\n'+ word_meaning[word_meanings_col.ADJECTIVE]+ '\n'+
-                        word_meaning[word_meanings_col.ADVERB]+ '\n'+ word_meaning[word_meanings_col.NOUN]+ '\n'+
-                        word_meaning[word_meanings_col.OTHERS];
+        let meaning =
+          word_meaning[word_meanings_col.VERB] +
+          "\n" +
+          word_meaning[word_meanings_col.ADJECTIVE] +
+          "\n" +
+          word_meaning[word_meanings_col.ADVERB] +
+          "\n" +
+          word_meaning[word_meanings_col.NOUN] +
+          "\n" +
+          word_meaning[word_meanings_col.OTHERS];
 
-          
-          /*this.setState({meaning_sc: meaning_sc, mnemonic_sc: mnemonic_sc, sentence_sc: sentence_sc, total: total,
+        /*this.setState({meaning_sc: meaning_sc, mnemonic_sc: mnemonic_sc, sentence_sc: sentence_sc, total: total,
           meaning_cor: false, mnemonic_cor: false, sentence_cor: false,
           submitted: false, submitting_test: false, index: index})*/
 
-          this.setState({
-            //New Word and its Details
-            index: index,
-            word: word_meaning[word_meanings_col.WORD],
-            wid: word_meaning[word_meanings_col.MAIN_ID],
-            meaning_ans: meaning,
-            mnemonic_ans: word_meaning[word_meanings_col.MNEMONIC],
-            sentence_ans: word_meaning[word_meanings_col.EXAMPLE],
+        this.setState({
+          //New Word and its Details
+          index: index,
+          word: word_meaning[word_meanings_col.WORD],
+          wid: word_meaning[word_meanings_col.MAIN_ID],
+          meaning_ans: meaning,
+          mnemonic_ans: word_meaning[word_meanings_col.MNEMONIC],
+          sentence_ans: word_meaning[word_meanings_col.EXAMPLE],
 
-            //Updated Scores
-            meaning_sc: meaning_sc, mnemonic_sc: mnemonic_sc, sentence_sc: sentence_sc, total: total,
+          //Updated Scores
+          meaning_sc: meaning_sc,
+          mnemonic_sc: mnemonic_sc,
+          sentence_sc: sentence_sc,
+          total: total,
 
-            //Others Reset to Default
-            meaning_cor: false, mnemonic_cor: false, sentence_cor: false,
-            submitted: false, submitting_test: false
-          })
-          }
-        
-        })
-  }
-  
+          //Others Reset to Default
+          meaning_cor: false,
+          mnemonic_cor: false,
+          sentence_cor: false,
+          submitted: false,
+          submitting_test: false,
+        });
+      }
+    });
+  };
+
   //When Submit Button is Pressed (of Question)
   submitHandler = () => {
     let submitted = !this.state.submitted;
-    this.setState({submitted: submitted});
-  }
+    this.setState({ submitted: submitted });
+  };
 
   //When Submit Result is Pressed  (of Answer)
   submitResultHandler = () => {
     //Update Scores
     let meaning_sc = this.state.meaning_sc;
-    if (this.state.meaning_cor){
-      console.log('Meaning is Right');
-      meaning_sc = meaning_sc+1;
+    if (this.state.meaning_cor) {
+      console.log("Meaning is Right");
+      meaning_sc = meaning_sc + 1;
     }
     let mnemonic_sc = this.state.mnemonic_sc;
     if (this.state.mnemonic_cor) {
-      console.log('Mnemonic is Right');
-      mnemonic_sc = mnemonic_sc+1;
+      console.log("Mnemonic is Right");
+      mnemonic_sc = mnemonic_sc + 1;
     }
     let sentence_sc = this.state.sentence_sc;
     if (this.state.sentence_cor) {
-      console.log('Sentence is Right');
-      sentence_sc = sentence_sc+1;
+      console.log("Sentence is Right");
+      sentence_sc = sentence_sc + 1;
     }
-    let total = this.state.total+1;
+    let total = this.state.total + 1;
 
     //Check if a word has failed, if so append the failed_words Redux list
-    if (((meaning_sc-this.state.meaning_sc)+(mnemonic_sc-this.state.mnemonic_sc)+(sentence_sc-this.state.sentence_sc))===0){
-      console.log(this.state.word, 'Failed');
-      this.props.appendFailedWord(this.state.word)
+    if (
+      meaning_sc -
+        this.state.meaning_sc +
+        (mnemonic_sc - this.state.mnemonic_sc) +
+        (sentence_sc - this.state.sentence_sc) ===
+      0
+    ) {
+      console.log(this.state.word, "Failed");
+      this.props.appendFailedWord(this.state.word);
     }
 
     //Update the word index
     let index;
     if (this.props.random) {
       this.props.removeRandomWordIndex(this.state.index);
-      let index_index = Math.floor(Math.random()*this.props.random_remaining_words.length)
-      index = this.props.random_remaining_words[index_index]
+      let index_index = Math.floor(
+        Math.random() * this.props.random_remaining_words.length
+      );
+      index = this.props.random_remaining_words[index_index];
     } else {
-      index = parseInt(this.state.index)+1;
+      index = parseInt(this.state.index) + 1;
     }
 
     //Reset the Corrections
@@ -146,7 +169,7 @@ class Test extends Component {
     /*this.setState({meaning_sc: meaning_sc, mnemonic_sc: mnemonic_sc, sentence_sc: sentence_sc, total: total,
       meaning_cor: false, mnemonic_cor: false, sentence_cor: false,
       submitted: false, submitting_test: false, index: index})*/
-  }
+  };
 
   //For the Answer class
   correctionHandler = (cur_result, result_type) => {
@@ -155,33 +178,33 @@ class Test extends Component {
     console.log(this.state);
     switch (result_type) {
       case strings.MEANING:
-        this.setState({meaning_cor: !cur_result});
+        this.setState({ meaning_cor: cur_result });
         break;
       case strings.MNEMONIC:
-        this.setState({mnemonic_cor: !cur_result});
+        this.setState({ mnemonic_cor: cur_result });
         break;
       case strings.SENTENCE:
-        this.setState({sentence_cor: !cur_result});
+        this.setState({ sentence_cor: cur_result });
         break;
     }
     //console.log(this.state);
-  }
+  };
 
   //When the Submit Test is Pressed
   submitTestHandler = () => {
-    this.setState({submitting_test: true});
-  }
+    this.setState({ submitting_test: true });
+  };
 
   //When the Submit Test Confirmation is Cancelled
   submitCancelHandler = () => {
-    this.setState({submitting_test: false});
-  }
+    this.setState({ submitting_test: false });
+  };
 
   //When the Submit Test Confirmation is Confirmed
   submitSuccessHandler = () => {
-    console.log('Getting you back to homepage');
+    console.log("Getting you back to homepage");
     //this.props.history.replace('/');
-    
+
     let history = {
       meaning: this.props.meaning,
       meaning_sc: this.state.meaning_sc,
@@ -191,24 +214,21 @@ class Test extends Component {
       sentence_sc: this.state.sentence_sc,
       random: this.props.random,
       index_start: this.props.index_start,
-      total: this.state.total
-    }
+      total: this.state.total,
+    };
 
     console.log(this.props.failed_words);
 
-    axios.post('/test/failed_words', this.props.failed_words)
-      .then(result => {
-        console.log(result);
-      })
+    axios.post("/test/failed_words", this.props.failed_words).then((result) => {
+      console.log(result);
+    });
 
     //Update history in the database
-    axios.post('/history/submit', history)
-      .then(result => {
-        console.log(result);
-        this.setState({submitted_test: true});
-      })
-
-  }
+    axios.post("/history/submit", history).then((result) => {
+      console.log(result);
+      this.setState({ submitted_test: true });
+    });
+  };
 
   render() {
     //console.log(this.state.index);
@@ -216,36 +236,84 @@ class Test extends Component {
 
     //If End Test has been clicked
     if (this.state.submitted_test) {
-      return <Redirect to="/"/>
+      return <Redirect to="/" />;
     }
 
     let answer = null;
     if (this.state.submitted) {
-      answer = <div><Answer meaning={this.props.meaning} mnemonic={this.props.mnemonic} sentence={this.props.sentence}
-                    meaning_ans={this.state.meaning_ans} mnemonic_ans={this.state.mnemonic_ans} sentence_ans={this.state.sentence_ans}
-                    meaning_cor={this.state.meaning_cor} mnemonic_cor={this.state.mnemonic_cor} sentence_cor={this.state.sentence_cor}
-                    correctionHandler={this.correctionHandler}/>
-                <Button disabled={false} btnType={ButtonClasses.DEFAULT} onClick={this.submitResultHandler}>Submit Result</Button>
-                <br></br></div>
+      answer = (
+        <div>
+          <Answer
+            meaning={this.props.meaning}
+            mnemonic={this.props.mnemonic}
+            sentence={this.props.sentence}
+            meaning_ans={this.state.meaning_ans}
+            mnemonic_ans={this.state.mnemonic_ans}
+            sentence_ans={this.state.sentence_ans}
+            meaning_cor={this.state.meaning_cor}
+            mnemonic_cor={this.state.mnemonic_cor}
+            sentence_cor={this.state.sentence_cor}
+            correctionHandler={this.correctionHandler}
+          />
+          <Button
+            disabled={false}
+            btnType={ButtonClasses.DEFAULT}
+            onClick={this.submitResultHandler}
+          >
+            Submit Result
+          </Button>
+          <br></br>
+        </div>
+      );
     }
 
-    let score = <Score meaning={this.props.meaning} mnemonic={this.props.mnemonic} sentence={this.props.sentence}
-                meaning_sc={this.state.meaning_sc} mnemonic_sc={this.state.mnemonic_sc} 
-                sentence_sc={this.state.sentence_sc} total={this.state.total} >
-                </Score>
+    let score = (
+      <Score
+        meaning={this.props.meaning}
+        mnemonic={this.props.mnemonic}
+        sentence={this.props.sentence}
+        meaning_sc={this.state.meaning_sc}
+        mnemonic_sc={this.state.mnemonic_sc}
+        sentence_sc={this.state.sentence_sc}
+        total={this.state.total}
+      ></Score>
+    );
 
     return (
       <Auxiliary>
-          {score}
-          <hr></hr>
-          <Modal show={this.state.submitting_test} modalClosed={this.submitCancelHandler}>
-            <EndTest cancel={this.submitCancelHandler} submit={this.submitSuccessHandler}></EndTest>
-          </Modal>
-          <Question word={this.state.word} meaning={this.props.meaning} mnemonic={this.props.mnemonic} sentence={this.props.sentence}/>
-          <Button disabled={false} btnType={ButtonClasses.DEFAULT} onClick={this.submitHandler}>Submit</Button>
-          <hr></hr>
-          {answer}
-          <Button disabled={false} btnType={ButtonClasses.SUCCESS} onClick={this.submitTestHandler}>End Test</Button>
+        {score}
+        <hr></hr>
+        <Modal
+          show={this.state.submitting_test}
+          modalClosed={this.submitCancelHandler}
+        >
+          <EndTest
+            cancel={this.submitCancelHandler}
+            submit={this.submitSuccessHandler}
+          ></EndTest>
+        </Modal>
+        <Question
+          word={this.state.word}
+          meaning={this.props.meaning}
+          mnemonic={this.props.mnemonic}
+          sentence={this.props.sentence}
+        />
+        <Button
+          disabled={false}
+          btnType={ButtonClasses.DEFAULT}
+          onClick={this.submitHandler}
+        >
+          Submit
+        </Button>
+        <hr></hr>
+        {answer}
+        <Button
+          disabled={false}
+          btnType={ButtonClasses.SUCCESS}
+          onClick={this.submitTestHandler}
+        >
+          End Test
+        </Button>
       </Auxiliary>
     );
   }
@@ -253,7 +321,7 @@ class Test extends Component {
   componentDidMount() {
     if (this.state.index) {
       this.getWord(0, 0, 0, 0, this.state.index);
-    } 
+    }
   }
 
   /*componentDidUpdate() {
@@ -262,11 +330,9 @@ class Test extends Component {
       this.getWord();
     }
   }*/
-
-
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     total_words: state.test.total_words,
     random: state.test.random,
@@ -275,15 +341,19 @@ const mapStateToProps = state => {
     mnemonic: state.test.mnemonic,
     sentence: state.test.sentence,
     failed_words: state.test.failed_words,
-    random_remaining_words: state.test.random_remaining_words
-  }
-}
+    random_remaining_words: state.test.random_remaining_words,
+  };
+};
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    appendFailedWord: (word) => {return dispatch(actions.appendFailedWord(word))},
-    removeRandomWordIndex: (index) => {return dispatch(actions.removeFromRandomList(index))}
-  }
-}
+    appendFailedWord: (word) => {
+      return dispatch(actions.appendFailedWord(word));
+    },
+    removeRandomWordIndex: (index) => {
+      return dispatch(actions.removeFromRandomList(index));
+    },
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Test));
